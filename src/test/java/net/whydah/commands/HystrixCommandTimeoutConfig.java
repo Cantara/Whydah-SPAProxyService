410f171169e5fa6031b92f82ada21d99bf6b6244
huy <huy@huy-HP2170p>
2018-03-09 14:36:59 +0700

    minor

---
+++ src/test/java/net/whydah/commands/HystrixCommandTimeoutConfig.java
@@ -0,0 +1,8 @@
+package net.whydah.commands;
+
+public class HystrixCommandTimeoutConfig {
+
+	public static int defaultTimeout = 30000000; //set for testing to get rid of fall-back possibility
+	
+	
+}
