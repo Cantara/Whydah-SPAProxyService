2b72eff64ec85acf023e3354e4a757b60bef33b1
Håkon Nymo Matland <haakon@nymomatland.com>
2019-01-21 15:52:10 +0100

    Initial commit of generic proxy functionality
    Inspired by https://github.com/Cantara/HTTPLoadTest-Baseline
    Intention is to enable generic usecases through
    json configuration files.

---
+++ src/main/java/net/whydah/service/httpproxy/Fizzler.java
@@ -0,0 +1,127 @@
+package net.whydah.service.httpproxy;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * https://github.com/Cantara/HTTPLoadTest-Baseline/blob/master/src/main/java/no/cantara/service/loadtest/Fizzler.java
+ */
+public class Fizzler {
+
+    private static Random r = new Random();
+
+    public static String getRandomDigits(int numberOfDigits) {
+        String result = "";
+        for (int n = 0; n < numberOfDigits; n++) {
+            result = result + getRandomDigit();
+        }
+        return result;
+    }
+
+    public static String generatePin() {
+        int i = r.nextInt(10000) % 10000;
+        java.text.DecimalFormat f = new java.text.DecimalFormat("0000");
+        return f.format(i);
+
+    }
+
+    public static char getRandomDigit() {
+        String abc = "0123456789";
+        return abc.charAt(r.nextInt(abc.length()));
+    }
+
+    public static char getRandomHEX() {
+        String abc = "0123456789ABCDEF";
+        return abc.charAt(r.nextInt(abc.length()));
+    }
+    public static char getRandomUppercaseCharacter() {
+        String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return abc.charAt(r.nextInt(abc.length()));
+    }
+
+    public static char getRandomLowercaseCharacter() {
+        String abc = "abcdefghijklmnopqrstuvwxyz";
+        return abc.charAt(r.nextInt(abc.length()));
+    }
+
+    public static char getRandomCharacter() {
+        String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        return abc.charAt(r.nextInt(abc.length()));
+    }
+
+    public static String getRandomCharacters(int noOfChars) {
+        String result = "";
+        for (int n = 0; n < noOfChars; n++) {
+            result = result + getRandomCharacter();
+        }
+        return result;
+    }
+
+    public static String getRandomLowercaseCharacter(int noOfChars) {
+        String result = "";
+        for (int n = 0; n < noOfChars; n++) {
+            result = result + getRandomLowercaseCharacter();
+        }
+        return result;
+    }
+
+    public static String getRandomUppercaseCharacter(int noOfChars) {
+        String result = "";
+        for (int n = 0; n < noOfChars; n++) {
+            result = result + getRandomUppercaseCharacter();
+        }
+        return result;
+    }
+
+    public static String getRandomHEXCharacter(int noOfChars) {
+        String result = "";
+        for (int n = 0; n < noOfChars; n++) {
+            result = result + getRandomHEX();
+        }
+        return result;
+    }
+
+    public static String getRandomSetValue(String options) {
+        String[] values = options.split(",");
+        int selected = r.nextInt(values.length);
+        return values[selected].trim();
+
+    }
+
+    // Quick un-escaping of json set
+    public static String getRandomSetValueAsString(String options) {
+        String[] values = options.split(",");
+        int selected = r.nextInt(values.length);
+        String jsonvalue = values[selected].trim();
+        String stringValue = jsonvalue.replaceAll("\\[", "").replaceAll("\"", "").replaceAll("]", "").trim();
+        return stringValue;
+
+    }
+
+    // #fizzle(substring(0,32):#testString)
+    public static String getSubString(String indices, String myString) {
+        String[] startAndStopIndex = indices.split("[, ]");
+        int startIndex=Integer.parseInt(startAndStopIndex[0]);
+        int stopIndex=Integer.parseInt(startAndStopIndex[1]);
+        String result = myString.substring(startIndex, stopIndex);
+        return result;
+
+    }
+
+    private static final ZoneId z = ZoneId.of("Z");
+    private static final DateTimeFormatter iso_timestamp_formattter
+            = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssX");
+    private static final Map<String, DateTimeFormatter> formatterByPattern = new ConcurrentHashMap<>();
+
+    public static String getTimestamp(String formatPattern) {
+        if (formatPattern == null || formatPattern.isEmpty()) {
+            return ZonedDateTime.now(z).format(iso_timestamp_formattter); // default
+        }
+        DateTimeFormatter formatter = formatterByPattern.computeIfAbsent(formatPattern, p -> DateTimeFormatter.ofPattern(p));
+        return ZonedDateTime.now(z).format(formatter);
+    }
+}
---
+++ src/main/java/net/whydah/service/httpproxy/GenericProxyResource.java
@@ -0,0 +1,133 @@
+package net.whydah.service.httpproxy;
+
+import net.whydah.service.CredentialStore;
+import net.whydah.service.SPAApplicationRepository;
+import net.whydah.service.auth.SPAKeyStoreRepository;
+import net.whydah.sso.application.types.ApplicationToken;
+import net.whydah.util.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.Optional;
+
+import static javax.ws.rs.core.Response.*;
+import static net.whydah.service.httpproxy.GenericProxyResource.API_PATH;
+
+
+@RestController
+@Path(API_PATH)
+@Produces(MediaType.APPLICATION_JSON)
+public class GenericProxyResource {
+    static final String API_PATH = "/httpproxy/generic";
+    private static final Logger log = LoggerFactory.getLogger(GenericProxyResource.class);
+
+    private static final String BEARER_TOKEN_PREFIX = "bearer";
+
+    private final CredentialStore credentialStore;
+    private final SPAApplicationRepository spaApplicationRepository;
+    private final SPAKeyStoreRepository spaKeyStoreRepository;
+    private final ProxySpecificationRepository proxySpecifications;
+
+    @Autowired
+    public GenericProxyResource(CredentialStore credentialStore, SPAApplicationRepository spaApplicationRepository,
+                                SPAKeyStoreRepository spaKeyStoreRepository, ProxySpecificationRepository proxySpecifications) {
+        this.credentialStore = credentialStore;
+        this.spaApplicationRepository = spaApplicationRepository;
+        this.spaKeyStoreRepository = spaKeyStoreRepository;
+        this.proxySpecifications = proxySpecifications;
+    }
+
+    /**
+     * Uses the userTokenId from path
+     */
+    @GET
+    @Path("{secret}/{userTokenId}/{proxySpecificationName}")
+    public Response getGeneric(@Context UriInfo uriInfo,
+                               @Context HttpHeaders httpheaders,
+                               @PathParam("secret") String secret,
+                               @PathParam("userTokenId") String userTokenId,
+                               @PathParam("proxySpecificationName") String proxySpecificationName) throws CloneNotSupportedException {
+
+        ApplicationToken applicationToken = spaApplicationRepository.getApplicationTokenBySecret(secret);
+        if (applicationToken == null) {
+            log.warn("Unable to locate application session from secret, returning FORBIDDEN");
+            return status(Status.FORBIDDEN).build();
+        }
+        log.debug("GET invoked with proxySpecificationName: {}", proxySpecificationName);
+        Response response = proxyRequest(proxySpecificationName, applicationToken, userTokenId, httpheaders);
+        return response;
+    }
+
+    /**
+     * Retrieves the userTokenId from the provided JWT
+     */
+    @GET
+    @Path("{secret}/{proxySpecificationName}")
+    public Response getGenericWithJWT(@Context UriInfo uriInfo,
+                                      @Context HttpHeaders httpheaders,
+                                      @PathParam("secret") String secret,
+                                      @PathParam("proxySpecificationName") String proxySpecificationName,
+                                      @HeaderParam("Authorization") String authorizationHeader) throws CloneNotSupportedException {
+
+        ApplicationToken applicationToken = spaApplicationRepository.getApplicationTokenBySecret(secret);
+        if (applicationToken == null) {
+            log.warn("Unable to locate application session from secret, returning FORBIDDEN");
+            return status(Status.FORBIDDEN).build();
+        }
+
+        log.debug("GET invoked with proxySpecificationName: {}", proxySpecificationName);
+        //read the authorization header
+        String userTokenId;
+        if (authorizationHeader != null && authorizationHeader.toLowerCase().startsWith(BEARER_TOKEN_PREFIX + " ")) {
+            String jwt = authorizationHeader.substring(BEARER_TOKEN_PREFIX.length()).trim();
+            userTokenId = spaKeyStoreRepository.getUserTokenIdFromJWT(jwt);
+            if (userTokenId == null || userTokenId.isEmpty()) {
+                return status(Status.FORBIDDEN).build();
+            }
+        } else {
+            return status(Status.FORBIDDEN).build();
+        }
+
+        return proxyRequest(proxySpecificationName, applicationToken, userTokenId, httpheaders);
+    }
+
+
+    private Response proxyRequest(final String proxySpecificationName,
+                                  final ApplicationToken applicationToken, final String userTokenId,
+                                  final HttpHeaders httpheaders) throws CloneNotSupportedException {
+        Optional<ProxySpecification> optionalSpecification = proxySpecifications.get(proxySpecificationName);
+        if (!optionalSpecification.isPresent()) {
+            log.info("ProxySpecification not found for targetName: {}, proxySpecificationName{}");
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        ProxySpecification specification = getCloneWithTokensSet(optionalSpecification.get(), applicationToken.getApplicationTokenId(), userTokenId);
+
+        Response response = new CommandGenericGetProxy(
+                specification,
+                httpheaders.getRequestHeaders()
+        ).execute();
+
+        return fromResponse(response)
+                .header("Access-Control-Allow-Origin", credentialStore.findRedirectUrl(applicationToken.getApplicationName()))
+                .header("Access-Control-Allow-Credentials", true)
+                .header("Access-Control-Allow-Headers", "*")
+                .build();
+    }
+
+    /**
+     * @param specification The {@link ProxySpecification} that will be cloned
+     * @param applicationTokenId will be added to the replacement map for the key #applicationTokenId
+     * @param userTokenId will be added to the replacement map for the key #userTokenId
+     * @return a clone of specification with the two replacement entries added
+     */
+    private ProxySpecification getCloneWithTokensSet(ProxySpecification specification, String applicationTokenId, String userTokenId) throws CloneNotSupportedException {
+        ProxySpecification clone = specification.clone();
+        clone.addEntryToCommand_replacement_map("#applicationTokenId", applicationTokenId);
+        clone.addEntryToCommand_replacement_map("#userTokenId", userTokenId);
+        return clone;
+    }
+}
